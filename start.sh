3ad9d4d8c3f308d8d0242ece8b91fac44578e44b
stanley.liang <stanley.liang@localhost.localdomain>
2016-09-02 18:09:04 -0400

    remove nexus dependency

--- start.sh
+++ start.sh
@@ -1,21 +1,12 @@
 #!/bin/bash -e
 
-if [ -d "tempWorkSpace" ]; then
-    rm -rf tempWorkSpace
-fi
-
-mkdir tempWorkSpace
-cd tempWorkSpace
-
 # setup host ip
-export mockhost=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | grep 10.)
+#export mockhost=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | grep 10.)
+export mockhost=localhost
 # setup workarea
 export tempwork=$(pwd)
+# clean tag file
+rm -f done.tag
 
-git clone git@gitsrc-us.ctbg.acer.com:stanley.liang/npt02.git
-
-cd npt02
 mvn clean test -Dtest.host=${host} -Dtest.mockhost=${mockhost} -Dtest.tempwork=$tempwork
 
-cd ../
-rm -rf tempWorkSpace
